0e9a4520cd10b67746ac1b5adf8641362d103919
Adam Gańczorz <iloscnd@gmail.com>
2019-01-14 18:52:12 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def confusion_matrix(prediction, true_vals, classes=None):
     for clss1 in classes:
         row = []
         for clss2 in classes:
-            row.append( ((true_vals == clss2) == (prediction == clss1)).sum()  )
+            row.append( np.logical_and((true_vals == clss2), (prediction == clss1)).sum()  )
         
         res.append(np.array(row))
     
